83fc61cc00a366427efffb349c696f0870fe950b
taimursiddiqui <taimur2013@namal.edu.pk>
2017-05-23 22:56:32 +0500

    added the reference

---
+++ app/static/js/bootstrap_tagspicker.js
@@ -0,0 +1,10 @@
+// this tag picker template is taken from this http://www.bootply.com/snippets/SqNK1FD2qK
+//Select2
+$.getScript('http://cdnjs.cloudflare.com/ajax/libs/select2/3.4.8/select2.min.js',function(){
+
+  /* Select2 plugin as tagpicker */
+  $("#tagPicker").select2({
+    closeOnSelect:false
+  });
+
+}); //script
